2777517d09834839b65d5ce29bce13544029bb0f
macnemoto <marcosj465@gmail.com>
2023-09-09 12:09:26 -0400

    last changes

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script setup>
+import axios from 'axios'
+
+axios.post('http://localhost:3000/account-recovery',{
+  date: '03/12/1992',
+  code: '4019',
+  unanswered: 'N',
+  familyLoad: '0',
+  dni: '20855785'
+})
+.then(res => {
+    console.log(res);
+  })
+.catch(error => {
+  console.log(error)
+})
+</script>
+
+<template>
+
+<h1>Hola Funciono</h1>
+
+</template>
+
+<style scoped>
+
+</style>
