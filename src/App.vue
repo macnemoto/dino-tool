6732ec4562476ec83a838b779c3a5417fca5a525
macnemoto <marcosj465@gmail.com>
2023-09-10 18:55:38 -0400

    :art: implement input first

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
- import HelloWorld from './components/HelloWorld.vue'
+import InputForm from './components/InputForm.vue'
 </script>
 
 <template>
   <div>
- <HelloWorld/>
+ <InputForm/>
   </div>
 </template>
 
---
+++ src/components/InputForm.vue
@@ -0,0 +1,46 @@
+<script setup>
+// import axios from 'axios'
+// axios.post('http://localhost:3000/account-recovery', {
+//   date: '03/12/1992',
+//   code: '4019',
+//   unanswered: 'N',
+//   familyLoad: '0',
+//   dni: '20855785'
+// })
+//   .then(res => {
+//     console.log(res)
+//   })
+//   .catch(error => {
+//     console.log(error)
+//   })
+
+import { watch, ref } from 'vue'
+const value = ref('')
+const value1 = ref('')
+watch(value, () => {
+  console.log(value.value)
+})
+watch(value1, () => {
+  console.log(value1.value)
+})
+</script>
+
+<template>
+  <a-form
+    layout="horizontal"
+    style="max-width: 600px"
+  >
+  <a-space direction="vertical">
+    <a-input v-model:value.lazy="value" autofocus placeholder="Cedula" />
+    <a-input v-model:value.lazy="value1"  placeholder="Cargar familiar" />
+    <a-input-number
+      :min="0"
+      :max="99999999"
+    />
+  </a-space>
+  </a-form>
+</template>
+
+<style scoped>
+
+</style>
