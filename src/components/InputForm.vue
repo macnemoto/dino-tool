0109944907cb1c6a177b03d511bc87bcefd8dc42
macnemoto <marcosj465@gmail.com>
2023-09-30 03:49:14 -0400

    :sparkles: added function to the modal button to send the captcha to the server

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -26,6 +26,7 @@ const unanswered = 'N'
 const open = ref(false)
 const base = 'data:image/png;base64,'
 const img = ref('')
+const captchaValue = ref('')
 
 watch(familyLoad, () => {
   console.log(familyLoad.value.toString())
@@ -63,62 +64,62 @@ const recoveryAccount = () => {
     })
 }
 
+watch(captchaValue, () => {
+  console.log(captchaValue.value)
+  axios.post('http://localhost:3000/captcha', {
+    captchaValue: captchaValue.value.toString()
+
+  })
+    .then(res => {
+      console.log(res)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
 </script>
 
 <template >
   <div>
     <div>
 
-<h2 class="text-4xl font-bold dark:text-white mb-20 ">Dino Tool</h2>
+      <h2 class="text-4xl font-bold dark:text-white mb-20 ">Dino Tool</h2>
 
-<img class="h-auto max-w-lg rounded-lg mb-10 " src="https://fresh.deno.dev/illustration/lemon-squash.svg?__frsh_c=5b73c33e2a2b5a2dac54486291c6495550cf5c4f" alt="image description">
-  </div>
-  <a-form
-    layout="horizontal"
-    style="max-width: 600px"
-  >
-  <a-space direction="vertical">
-    <!-- <a-input v-model:value.lazy="value1" autofocus placeholder="Cargar familiar" /> -->
-    <a-input-number class="w-48 block mb-2 text- text-gray-900 font-bold"
-    autofocus
-      :min="0"
-      :max="20"
-      :maxlength="2"
-      placeholder="Cargar familiar"
-      v-model:value.lazy="familyLoad"
-    />
-    <a-input-number class="w-48 block mb-2 text- text-gray-900 font-bold"
-      :min="0"
-      :max="99999999"
-      placeholder="Cedula"
-      :maxlength="8"
-      v-model:value.lazy="dni"
-    />
-    <a-input-number class="w-48 block mb-2 text- text-gray-900 font-bold"
-      :min="0"
-      :max="9999"
-      placeholder="Codigo Postal"
-      :maxlength="4"
-      v-model:value.lazy="code"
-    />
-    <a-date-picker class="w-48 block mb-2 text- text-gray-900 font-bold" v-model:value="date" :valueFormat="dateFormat" :format="dateFormat"  />
-    <a-button class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm text-center mr-2 mb-2" @click="recoveryAccount" type="primary">Recuperar Cuenta</a-button>
-  </a-space>
-  </a-form>
-  <a-modal v-model:open="open" title="Basic Modal" >
-    <figure class="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
-  <a href="#">
-    <img class="rounded-lg" :src="base + img" alt="image description">
-  </a>
-  <figcaption class="absolute px-4 text-lg text-white bottom-6">
-      <p>Por favor rellene el Capchat</p>
-  </figcaption>
-</figure>
-      <p>Some contents...</p>
+      <img class="h-auto max-w-lg rounded-lg mb-10 "
+        src="https://fresh.deno.dev/illustration/lemon-squash.svg?__frsh_c=5b73c33e2a2b5a2dac54486291c6495550cf5c4f"
+        alt="image description">
+    </div>
+    <a-form layout="horizontal" style="max-width: 600px">
+      <a-space direction="vertical">
+        <!-- <a-input v-model:value.lazy="value1" autofocus placeholder="Cargar familiar" /> -->
+        <a-input-number class="w-48 block mb-2 text- text-gray-900 font-bold" autofocus :min="0" :max="20" :maxlength="2"
+          placeholder="Cargar familiar" v-model:value.lazy="familyLoad" />
+        <a-input-number class="w-48 block mb-2 text- text-gray-900 font-bold" :min="0" :max="99999999"
+          placeholder="Cedula" :maxlength="8" v-model:value.lazy="dni" />
+        <a-input-number class="w-48 block mb-2 text- text-gray-900 font-bold" :min="0" :max="9999"
+          placeholder="Codigo Postal" :maxlength="4" v-model:value.lazy="code" />
+        <a-date-picker class="w-48 block mb-2 text- text-gray-900 font-bold" v-model:value="date"
+          :valueFormat="dateFormat" :format="dateFormat" />
+        <a-button
+          class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm text-center mr-2 mb-2"
+          @click="recoveryAccount" type="primary">Recuperar Cuenta</a-button>
+      </a-space>
+    </a-form>
+    <a-modal v-model:open="open" okType="primary" >
+      <a-space direction="vertical" class="flex justify-center items-center ">
+        <p class="text-lg font-bold">Eres un robot, bí-bú? </p>
+          <a href="#">
+            <img class="rounded-lg" :src="base + img" alt="image description">
+          </a>
+          <a-input v-model:value.lazy="captchaValue" autofocus placeholder="Escriba el captcha" />
+        </a-space>
     </a-modal>
   </div>
 </template>
 
 <style scoped>
-
+.ant-modal-content{
+background-color:#77dd77 !important ;
+}
 </style>
